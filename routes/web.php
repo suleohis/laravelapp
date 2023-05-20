7ed90d3fc6a077937ca952f83dc00a642834e45c
suleohis <suleephraim1@gmai.com>
2023-05-20 17:43:15 +0100

    first Commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\FallbackController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('/blog')->group(function () {
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+    Route::patch('/{id}', [PostsController::class, 'destroy'])->name('blog.destory');
+});
+
+// Route::resource('blog', PostsController::class);
+
+// Route for invoke method
+Route::get('/', HomeController::class);
+
+// Multiple HTTP verbs
+// Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+// Route::any('/blog', [PostsController::class, 'index']);
+
+// Return view
+// Route::view('/blog', 'blog.index', ['name' => 'Code With Ohis']);
+
+//Fallback Route
+Route::fallback(FallbackController::class);
